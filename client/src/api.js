be853e30ac0c22c1e925a1afad4e94ec8190c0ac
sharmaasahill <i.sahilkrsharma@gmail.com>
2025-09-15 05:25:30 +0530

    Fix: Auto-append /api to REACT_APP_API_URL if missing

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000/api",
+    baseURL: process.env.REACT_APP_API_URL ? 
+        (process.env.REACT_APP_API_URL.endsWith('/api') ? 
+            process.env.REACT_APP_API_URL : 
+            process.env.REACT_APP_API_URL + '/api') : 
+        "http://localhost:5000/api",
 });
 
 // Debug: Log the API URL being used
